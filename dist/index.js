a1083488da78699ed2c3824aa43d78a8919957f2
Azka Azeem <azkaazeem804@gmail.com>
2025-11-29 11:31:57 +0500

    configration done

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+export {};
+/*
+1. Sb sy phly node js check kro h ye command likh kr terminal -> cmd mn = node -v
+  -> agr koi version aaya tu it means k download h otherwise download node js
+
+2. then check kro npm h using this command = npm -v
+  -> isy download krny ki need nhi is ka bs version check krty hn
+
+3. third no pr hamen tsc version check krna hota h, using this command = tsc -v
+  -> agr koi version aaya tu it means k download h otherwise download typescript..
+      = typescript is command sy download ho gi = npm i -g typescript (ts download kny k bad check krna k hoi ya nhi using this command = tsc -v)
+
+4.
+ */ 
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/*
+1. Sb sy phly node js check kro h ye command likh kr terminal -> cmd mn = node -v
+  -> agr koi version aaya tu it means k download h otherwise download node js
+
+2. then check kro npm h using this command = npm -v
+  -> isy download krny ki need nhi is ka bs version check krty hn
+
+3. third no pr hamen tsc version check krna hota h, using this command = tsc -v
+  -> agr koi version aaya tu it means k download h otherwise download typescript..
+      = typescript is command sy download ho gi = npm i -g typescript (ts download kny k bad check krna k hoi ya nhi using this command = tsc -v)
+
+4. phir hamen "tsconfig.json" ki file banani h ye code likh kr = tsc --init (is sy ak file bn jay gi)
+
+5. file k andr ja kr File Layout mn (rootDir & OutDir) un commit krna h aur phir Environment Settings mn ja kr module mn nodenext ko replace krna h and target mn es next ko replace kr k es ka wo version likhna h jo sahi lagy e.g (es2015, es2016, es2017 etc.) zada popular es2016. 
+
+6. then phir do folder bana 
+ */
